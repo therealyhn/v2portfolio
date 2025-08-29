b21113714b880704a00bbba6bf479f3cf588dfc7
Jovan Ljusic <jovanljusicum@outlook.com>
2025-08-29 12:38:03 +0200

    init: setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-600 dark:text-blue-400">
+        Hello Tailwind v3 + Vite + React 🚀
+      </h1>
+    </div>
+  )
+}
