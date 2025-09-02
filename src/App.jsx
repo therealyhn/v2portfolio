0c5376f1fab226f50e18d14760bfec34b2b7fc98
Jovan Ljusic <jovanljusicum@outlook.com>
2025-09-02 10:40:10 +0200

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./sections/Navbar";
 import Home from "./sections/Home";
 import { Suspense } from "react";
-// import { lazy } from "react";
+import { lazy } from "react";
 
-// const About = lazy(() => import("./sections/About"));
+const About = lazy(() => import("./sections/About"));
 // const Projects = lazy(() => import("./sections/Projects"));
 // const Contact = lazy(() => import("./sections/Contact"));
 
@@ -15,6 +15,7 @@ export default function App() {
 
       {/* Lazy load sekcija */}
       <Suspense fallback={<div className="text-center py-20 text-gray-400">Loading...</div>}>
+        <About />
       </Suspense>
     </div>
   );
