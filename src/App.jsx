6a591411e981fe76a223de0174af4c88266b6222
Jovan Ljusic <jovanljusicum@outlook.com>
2026-01-22 10:15:23 +0100

    pricing removed

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
           />
 
           {/* Posebna stranica samo za cenovnik */}
-          <Route
+          {/* <Route
             path="/pricing"
             element={
               <>
@@ -48,7 +48,7 @@ export default function App() {
                 <Footer />
               </>
             }
-          />
+          /> */}
         </Routes>
       </Suspense>
     </div>
